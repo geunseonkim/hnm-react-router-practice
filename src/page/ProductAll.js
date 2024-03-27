b757a8d0be416f54944c56ff72c6d9b10e75a5ff
geunseonkim <guensoen@gmail.com>
2024-03-27 12:22:32 -0700

    change url

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -6,7 +6,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 const ProductAll = () => {
   const [productList, setProductList] = useState([]); //ui에 불러주려면 useState!
   const getProducts = async() => {
-    let url = `http://localhost:4000/products`
+    let url = `https://my-json-server.typicode.com/geunseonkim/hnm-react-router-practice/products`
     let response = await fetch(url)
     let data = await response.json();
     setProductList(data);
