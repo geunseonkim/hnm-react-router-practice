27661e17af0d665e2108543be3ad42427bbb00c9
geunseonkim <guensoen@gmail.com>
2024-03-28 13:14:11 -0700

    login page redirect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import ProductAll from './page/ProductAll'
 import Login from './page/Login'
-import ProductDetail from './page/ProductDetail'
+//import ProductDetail from './page/ProductDetail'
 import Navbar from './component/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PrivateRoute from './route/PrivateRoute'
 
 function App() {
   const[authenticate, setAuthenticate] = useState(false) // true: login success
+  useEffect(() => {
+    console.log("aaa", authenticate);
+  }, [authenticate])
   return (
     <div >
       <Navbar/>
       <Routes>
         <Route path="/" element={<ProductAll/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/product/:id" element={<ProductDetail/>}/>
+        <Route path="/login" element={<Login setAuthenticate={setAuthenticate}/>}/>
+        <Route path="/product/:id" element={<PrivateRoute authenticate={authenticate}/>}/>
       </Routes>
     </div>
   );
